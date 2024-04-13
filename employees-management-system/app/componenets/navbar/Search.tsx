7b00dc35625836310689b20aa709ab1f9876ff7f
abedalawieh <abedalaweye@gmail.com>
2024-04-13 16:27:35 +0300

    Created the navbar components

---
+++ employees-management-system/app/componenets/navbar/Search.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { BiSearch } from "react-icons/bi";
+
+const Search = () => {
+  return (
+    <div className=" border-[1-px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+      <div className="flex flex-row items-center justify-between ">
+        <div className="text-sm font-semibold px-6">Search by name</div>
+        <div className="text-sm font-semibold px-6">Search by location</div>
+        <div className="text-sm font-semibold px-6">
+          <BiSearch size={18} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
